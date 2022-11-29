d065e2d25a546c171738d9fe3396014017295ab9
Facundo Vergara <fakumax@gmail.com>
2022-11-29 20:24:29 -0300

    Done

--- src/screen/AppNavigation/NoTabBarStack.js
+++ src/screen/AppNavigation/NoTabBarStack.js
@@ -4,43 +4,57 @@ const Stack = createNativeStackNavigator();
 
 import { SettingScreen } from '../TabHome/SettingScreen';
 import { SettingsProfile } from '../TabProfile/SettingsProfile';
+import { Image, Button } from 'react-native';
 
 const screenOptionStyle = {
   headerStyle: {
-    elevation: 0, //Elimina la linea del header Android
-    shadowColor: 'transparent', //Elimina la linea del header IOS
+    elevation: 20, //Elimina la linea del header Android
+    shadowColor: 'red', //Elimina la linea del header IOS
     // cardStyle: {
     //   backgroundColor: APP_COLOR_SECONDARY,
     // },
-    // header: { visible: false },
-    headerMode: 'screen',
-    // title: '',
+    // //headerMode: 'screen',
+    title: '',
   },
+  //headerLeft: (props) => <LogoTitle {...props} />,
   headerTransparent: false,
-  // headerTitle: 'LOGIN',
+  ///////////////////////////
+  headerRight: () => <Button title='Update count' />,
+  //headerTitle: 'LOGIN',
   // headerLeft : null,
-  // headerRight: null,
-  // headerTintColor: APP_WHITE,
+  //   headerTintColor: 'blue',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+    color: 'blue',
+  },
   // navigationOptions:{
   //   title: 'Header title'
   // }
 };
 
+function LogoTitle() {
+  return (
+    <Image
+      style={{ width: 50, height: 50 }}
+      source={{
+        uri: 'https://1drv.ms/u/s!AiyE3dAIjI8lgRtQot0nBaNTeCLb?e=O2r7fz',
+      }}
+    />
+  );
+}
+
 const NoTabBarStack = () => {
   return (
-    <Stack.Navigator
-
-    // screenOptions={screenOptionStyle}
-    >
+    <Stack.Navigator screenOptions={screenOptionStyle}>
       {/* <Stack.Screen name='Login' component={Login} options={{ title: '', headerMode: 'none' }}/> */}
       <Stack.Screen
         name='SettingScreen'
         component={SettingScreen}
-        options={{
-          headerShown: true,
-          //   tabBarStyle: { display: 'none' },
-          //   tabBarVisible: false,
-        }}
+        //options={{
+        //headerShown: true,
+        //   tabBarStyle: { display: 'none' },
+        //   tabBarVisible: false,
+        //}}
       />
       <Stack.Screen
         name='SettingProfile'
